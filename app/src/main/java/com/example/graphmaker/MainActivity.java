f35c6d1917b69073f793f4974b243a2042874570
kshitiz-kumar <kkrajput09@gmail.com>
2019-03-03 16:04:15 +0530

    intial commit

---
+++ app/src/main/java/com/example/graphmaker/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.graphmaker;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        RealtimeUpdates fragment = new RealtimeUpdates();
+        fragmentTransaction.add(R.id.thisis, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
